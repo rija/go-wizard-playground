56955a69de5e9ab736b62bf4fba23fd5b3dbc49d
Rija Ménagé <rm.dev.git.commits@eml.cc>
2024-03-01 19:58:10 +0100

    implemented up to 8:32 of the video

--- main.go
+++ main.go
@@ -3,9 +3,23 @@ package main
 import (
 	"log"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
-type model struct {}
+
+type model struct {
+	index int
+	questions []string
+	width     int
+	height    int
+	answerField textinput.Model
+}
+
+func New(questions []string) *model {
+	answerField := textinput.New()
+	return &model{questions: questions, answerField: answerField}
+}
 
 func (m model) Init() tea.Cmd {
 	return nil
@@ -13,28 +27,39 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-		case tea.KeyMsg:
-			switch msg.String() {
-				case "ctrl+c":
-					return m, tea.Quit
-			}
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		}
 	}
 	return m, nil
 }
 
 func (m model) View() string {
-	return "Hello World"
+	if m.width == 0 {
+		return "Loading..."
+	}
+	return lipgloss.JoinVertical(lipgloss.Center, m.questions[m.index], m.answerField.View())
 }
 
 func main() {
-	f, err := tea.LogToFile("debug.log","debug")
+
+	questions := []string{"What is your name?","What is your favourite editor?", "What is your favour quote?"}
+
+	m := New(questions)
+
+	f, err := tea.LogToFile("debug.log", "debug")
 	if err != nil {
-		log.Fatalf("err: %W", err)
+		log.Fatal(err)
 	}
 	defer f.Close()
 
-	p := tea.NewProgram(model{}, tea.WithAltScreen())
-	if _, err := p.Run() ; err != nil {
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
 
