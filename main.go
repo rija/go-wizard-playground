27e91511637ef8899aecbe269c35f4c233720836
Rija Ménagé <rm.dev.git.commits@eml.cc>
2024-03-02 19:52:44 +0100

    implemented up to 12:53 of the video

--- main.go
+++ main.go
@@ -8,17 +8,36 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+type Styles struct {
+	BorderColor lipgloss.Color
+	InputField  lipgloss.Style
+}
+
+func DefaultStyles() *Styles {
+	s := new(Styles)
+	s.BorderColor = lipgloss.Color("36")
+	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
+	return s
+}
+
 type model struct {
-	index int
-	questions []string
-	width     int
-	height    int
+	index       int
+	questions   []string
+	width       int
+	height      int
 	answerField textinput.Model
+	styles      *Styles
 }
 
 func New(questions []string) *model {
+	styles := DefaultStyles()
 	answerField := textinput.New()
-	return &model{questions: questions, answerField: answerField}
+	answerField.Placeholder = "Your answer here"
+	return &model{
+		questions:   questions,
+		answerField: answerField,
+		styles:      styles,
+	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -43,12 +62,27 @@ func (m model) View() string {
 	if m.width == 0 {
 		return "Loading..."
 	}
-	return lipgloss.JoinVertical(lipgloss.Center, m.questions[m.index], m.answerField.View())
+	return lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		lipgloss.JoinVertical(
+			lipgloss.Center,
+			m.questions[m.index],
+			m.styles.InputField.Render(m.answerField.View()),
+		),
+	)
+
 }
 
 func main() {
 
-	questions := []string{"What is your name?","What is your favourite editor?", "What is your favour quote?"}
+	questions := []string{
+		"What is your name?",
+		"What is your favourite editor?",
+		"What is your favour quote?",
+	}
 
 	m := New(questions)
 
